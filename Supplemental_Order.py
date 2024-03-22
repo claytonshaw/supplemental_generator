64ea2be5042ff1044ee5674d746837917592b66b
claytonshaw <77822750+claytonshaw@users.noreply.github.com>
2024-03-22 15:55:12 -0600

    Update Supplemental_Order.py

--- Supplemental_Order.py
+++ Supplemental_Order.py
@@ -14,38 +14,38 @@ def blank_rows(file_path):
     return blank_rows
 
 
-def supplemental_order(file_upload, inventory_upload, use_custom_vendor_packs=False, vendor_packs_to_send=0,
-                       use_available=False, sort_by_zero_oh=True, just_find_need=False, weeks_forecast=6):
+def supplemental_order(file_upload,inventory_upload,use_custom_vendor_packs=False, vendor_packs_to_send=0,
+                       use_available=False,sort_by_zero_oh=True,just_find_need=False, weeks_forecast=6):
     import pandas as pd
     import numpy as np
     import warnings
-    warnings.filterwarnings('ignore')                       
+    warnings.filterwarnings('ignore')
     df = pd.read_excel(file_upload, skiprows=blank_rows(file_upload))
 
     unwanted_states = ['AK','HI','PR']
     df = df[~df['State'].isin(unwanted_states)]
 
-    available_inventory = pd.read_csv(inventory_upload) # use this dataset if you want to customize the on hand
+    available_inventory = pd.read_excel(inventory_upload) # inventory upload
 
     # getting the unique list of SKU's and saving it to dataframe
     unique_sku = df['Vendor Stk Nbr'].unique()
 
     # creating units needed calculation
-    df['6_wk_fcst'] = df.iloc[:,16:22].sum(axis=1) # summing each weeks forecast to get a total 6 week forecast
-    df['pipe_minus_fcst'] = df['PIPELINE'] - df['6_wk_fcst'] # getting pipeline minus forecast
+    df['wk_fcst'] = df.iloc[:,16:weeks_forecast+16].sum(axis=1) # summing each weeks forecast to get a total 6 week forecast
+    df['pipe_minus_fcst'] = df.iloc[:, 15] - df['wk_fcst'] # getting pipeline minus forecast
     df['pipe_minus_fcst'] = df.apply(lambda row: 0 if row['pipe_minus_fcst'] > 0 else row['pipe_minus_fcst'], axis=1) # if pipeline minus forecast is greater than 0 make it 0
     df['pipe_minus_fcst'] = df['pipe_minus_fcst'].abs() # getting absolute value of number
-    df['whse_pks_needed'] = df['pipe_minus_fcst'] / df['VNPK Qty'] # converting to vendor packs
+    df['whse_pks_needed'] = df['pipe_minus_fcst'] / df['Vnpk Qty'] # converting to vendor packs
     df['whse_pks_needed'] = df['whse_pks_needed'].apply(np.ceil) # rounding up to the nearest whole number
     if use_custom_vendor_packs:
         df['pipe_need'] = df.apply(lambda row: vendor_packs_to_send if row['whse_pks_needed'] > vendor_packs_to_send else row['whse_pks_needed'], axis=1)
-        df['pipe_need'] = df.apply(lambda row: vendor_packs_to_send if (row['6_wk_fcst'] == 0 and row['Curr Str On Hand Qty'] == 0) else row['pipe_need'], axis=1) # if the store's forecast and on hand is zero make the units need max shelf minus pipeline
+        df['pipe_need'] = df.apply(lambda row: vendor_packs_to_send if (row['wk_fcst'] == 0 and row.iloc[11] == 0) else row['pipe_need'], axis=1) # if the store's forecast and on hand is zero make the units need max shelf minus pipeline
     else:
-        df['pipe_need'] = df['whse_pks_needed'] * df['VNPK Qty'] # converting back to units
-        df['mx_shelf_minus_pipeline'] = df.apply(lambda row: 0 if row['Max Shelf Qty'] - row['PIPELINE'] < 0 else row['Max Shelf Qty'] - row['PIPELINE'], axis=1) # if max shelf qty minus pipe is les than 0 make it zero other wise make it the difference between max shelf and the pipe
-        df['pipe_need'] = df.apply(lambda row: row['Max Shelf Qty'] if row['pipe_need'] > row['Max Shelf Qty'] else row['pipe_need'], axis=1) # if the max shelf qty is less than the needed amount just make it max shelf qty
-        df['pipe_need'] = df.apply(lambda row: row['mx_shelf_minus_pipeline'] if (row['6_wk_fcst'] == 0 and row['Curr Str On Hand Qty'] == 0) else row['pipe_need'], axis=1) # if the store's forecast and on hand is zero make the units need max shelf minus pipeline
-        df['pipe_need'] = df['pipe_need'] / df['VNPK Qty'] # converting to vendor packs
+        df['pipe_need'] = df['whse_pks_needed'] * df['Vnpk Qty'] # converting back to units
+        df['mx_shelf_minus_pipeline'] = df.apply(lambda row: 0 if row.iloc[5] - row.iloc[15] < 0 else row.iloc[5] - row.iloc[15], axis=1) # if max shelf qty minus pipe is les than 0 make it zero other wise make it the difference between max shelf and the pipe
+        df['pipe_need'] = df.apply(lambda row: row.iloc[5] if row['pipe_need'] > row.iloc[5] else row['pipe_need'], axis=1) # if the max shelf qty is less than the needed amount just make it max shelf qty
+        df['pipe_need'] = df.apply(lambda row: row['mx_shelf_minus_pipeline'] if (row['wk_fcst'] == 0 and row.iloc[11] == 0) else row['pipe_need'], axis=1) # if the store's forecast and on hand is zero make the units need max shelf minus pipeline
+        df['pipe_need'] = df['pipe_need'] / df['Vnpk Qty'] # converting to vendor packs
         df['pipe_need'] = df['pipe_need'].apply(np.ceil) # rounding up to the nearest whole number
 
     #creating an empty dataframe for the loop
@@ -53,15 +53,15 @@ def supplemental_order(file_upload, inventory_upload, use_custom_vendor_packs=Fa
 
     for item in unique_sku:
         df_filtered = df[df['Vendor Stk Nbr'] == item]
-        df_filtered = df_filtered[df_filtered.iloc[:,6] == 1] # filtering to only valid stores
+        df_filtered = df_filtered[df_filtered.iloc[:, 6] == 1] # filtering to only valid stores
         df_filtered = df_filtered[df_filtered['Store Type Descr'] != 'BASE STR Nghbrhd Mkt'] # filtering out Neighborhood Market stores
         if sort_by_zero_oh:
-            curr_str_on_hand_qty = df_filtered.columns[11]
-            df_filtered = df_filtered.sort_values(by = [curr_str_on_hand_qty, 'pipe_need'], ascending=[False, False]).reset_index() # sort by stores that have zero on hand and pipe need
+            curr_str_on_hand_qy = df_filtered.iloc[:, 11]
+            df_filtered = df_filtered.sort_values(by = [curr_str_on_hand_qy, 'pipe_need'], ascending=[False, False]).reset_index() # sort by stores that have zero on hand and pipe need
         else:
             df_filtered = df_filtered.sort_values('pipe_need', ascending=False).reset_index() # sorting to rank stores with the highest pipe_need
 
-        # getting available inventory (next step would be to link this to domo so that if you don't want to use a custom dataset you won't need to upload anything. 
+        # getting available inventory 
         blkst_oh = available_inventory[available_inventory['Item'] == item]
         if just_find_need:
             available_inv = float(1000000) # makes the on hand qty 1 million so we can find the need and not be limited by what is on hand
